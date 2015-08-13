69043c3da7a7e3162f1dcaba1761cd236248eeb4
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-08-13 12:46:51 -0600

    Initial Commit

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,33 @@
+class PinsController < ApplicationController
+
+def index
+
+end
+
+def new
+
+	@pin = Pin.new
+
+end
+
+def create
+
+	@pin = Pin.new(pin_params)
+	
+	if @pin.save
+		redirect_to @pin, notice: "Succesfully created your pin."
+	else
+		render 'new'
+	end
+end
+
+private
+
+def pin_params
+
+	params.require(:pin).permit(:title, :description)
+
+end
+
+
+end
